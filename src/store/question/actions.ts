e0d37b04e2bd603a36d70e065c21b0b953b0b98c
hellomac87 <hellomac87@gmail.com>
2019-04-02 16:07:17 +0900

    add comments

--- src/store/question/actions.ts
+++ src/store/question/actions.ts
@@ -1,7 +1,9 @@
 import { CREATE_QUESTION, IcreateQuestion } from "./types";
 
+// type export for container
 export type createQuestionAction = IcreateQuestion;
 
+// action creator
 export const createQuestion = (): IcreateQuestion => {
   return {
     type: CREATE_QUESTION
--- src/store/question/types.ts
+++ src/store/question/types.ts
@@ -1,8 +1,11 @@
+// declare reducer state type
 export type QuestionState = number[];
 
+// constants for actions & reducers
 export const CREATE_QUESTION = "CREATE_QUESTION";
 export type CREATE_QUESTION = typeof CREATE_QUESTION;
 
+// interface for actions
 export interface IcreateQuestion {
   type: CREATE_QUESTION;
 }
